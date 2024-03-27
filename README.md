48ed962cabbcd964eb1c29edb3188139c6b5f206
Manik Khandelwal <mkhandelwal@microsoft.com>
2024-03-27 19:21:35 +0530

    initial commit -- remove keys

---
+++ README.md
@@ -0,0 +1,32 @@
+# Starter Package for @azure/cosmos JS SDK
+
+## General Starter Instructions
+
+This project is built using Node.js and TypeScript. Please ensure a recent version of Node and TypeScript is installed on your system.
+To set up the project, follow these steps:
+
+1. Install the dependencies:
+    ```bash
+    npm install
+    ```
+
+2. Build the project:
+    ```bash
+    npm run build
+    ```
+
+    Note: You need to run this command after each change.
+
+3. Check all variables mentioned in the `.env` file and update them accordingly.
+
+4. If you want to make custom amendments, go through the `./src/diagnostics-collector.json` file and update the necessary changes.
+
+5. Execute the script:
+    ```bash
+    npm run start
+    ```
+
+6. Collect the `diagnostics.json` file generated in the root folder and share it with us. 
+    Note: The `diagnostics.json` file can be found in the root folder of the project.
+
+
---
+++ src/diagnostics-collector.ts
@@ -0,0 +1,50 @@
+import { CosmosClient, CosmosDbDiagnosticLevel } from "@azure/cosmos";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const endpoint = process.env.COSMOS_ENDPOINT;
+const key = process.env.COSMOS_KEY ;
+const client = new CosmosClient({ endpoint, key, diagnosticLevel: CosmosDbDiagnosticLevel.debug});
+const diaganosticsPath = "diagnostics.json";
+
+
+async function main(): Promise<void> {
+    const { database } = await client.databases.createIfNotExists({ id: process.env.DATABASE});
+    const { container } = await database.containers.createIfNotExists({ id: process.env.CONTAINER });
+
+    const query = "SELECT * FROM c";
+    // Add abort controller
+    const controller = new AbortController();
+    const signal = controller.signal;
+    // Abort the query after n seconds
+    setTimeout(() => controller.abort(), parseInt(process.env.QUERY_ABORT_TIME_MS));
+    const queryOptions = {  signal ,
+        enableCrossPartitionQuery: true,
+        enableScanInQuery: true,
+        forceQueryPlan: true,
+        maxDegreeOfParallelism: 6,
+        maxItemCount: 100,
+        populateQueryMetrics: true
+    };
+
+    const queryIterator = container.items.query(query, queryOptions);
+    let result = 0;
+
+    try {
+        const { diagnostics} = await queryIterator.fetchNext({ruCapPerOperation: parseInt(process.env.RU_THRESOLD)});
+        saveDiagnosticsToFile(diagnostics, diaganosticsPath);
+    } catch (error) {
+        saveDiagnosticsToFile(error.diagnostics, diaganosticsPath);
+    }
+}
+
+function saveDiagnosticsToFile(diagnostics: any, filePath: string): void {
+    const json = JSON.stringify(diagnostics, null, 2);
+    fs.writeFileSync(filePath, json);
+}
+
+main().catch((error) => {
+    console.error(error);
+});
